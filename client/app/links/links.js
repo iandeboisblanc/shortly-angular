a1f5bb7d3687c99d6642f9aa518a9000a446da92
iandeboisblanc <iandeboisblanc@gmail.com>
2016-02-12 15:47:34 -0800

    Fill data object with links data when linksController is initialized

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,6 +1,8 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  Links.getAll();
   $scope.data = {};
+  Links.getAll().then(function (links) {
+    $scope.data.links = links;
+  });
 });
