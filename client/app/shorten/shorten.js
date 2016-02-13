0395c30ec3ef0be77a9e96d2bd3783570e8e90b3
iandeboisblanc <iandeboisblanc@gmail.com>
2016-02-12 20:00:11 -0800

    Implement minor changes

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,8 +3,7 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
   $scope.addLink = function () {
-    //check if a mothafucka is valid and display that shit
-    if($scope.shortForm.$valid) {
+    if ($scope.shortForm.$valid) {
       console.log('form is VALID!!');
     }
     Links.addOne($scope.link);
