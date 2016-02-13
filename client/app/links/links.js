fd472c16cfa923565451f9c1c2145ea155f37d61
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-02-13 11:51:23 -0800

    Add filtering function to links page

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,6 +1,13 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
+  $scope.nameContains = function (link) {
+    var text = $scope.filterInput;
+    var title = link.title;
+    var url = link.url;
+    console.log(text);
+    return true;
+  };
   $scope.data = {};
   Links.getAll().then(function (links) {
     $scope.data.links = links;
