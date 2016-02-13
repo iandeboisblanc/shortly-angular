fb4e37da5b58bdd1b6e9c88d14adf52a1e7e0594
iandeboisblanc <iandeboisblanc@gmail.com>
2016-02-12 19:59:28 -0800

    Check validity of links prior to submission

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,10 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
   $scope.addLink = function () {
+    //check if a mothafucka is valid and display that shit
+    if($scope.shortForm.$valid) {
+      console.log('form is VALID!!');
+    }
     Links.addOne($scope.link);
     $scope.link.url = '';
   };
